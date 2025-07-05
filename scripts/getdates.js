757a1b21a560cc2d477f49562949e2b318819284
agerockhub <agerock2013@gmail.com>
2025-07-05 11:24:48 +0000

    page completed

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+  // Set the current year
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+    // Set the last modified date
+    document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
