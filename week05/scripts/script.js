037443bce713eadfdc2e09ff6a0a59c408074be1
agerockhub <agerock2013@gmail.com>
2025-07-31 15:13:23 +0000

    updated

---
+++ week05/scripts/script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add('delete');
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    } else {
+        alert('Please enter a book and chapter (e.g., Alma 5).');
+    }
+});
+
+
+let chaptersArray = getChapterList();
